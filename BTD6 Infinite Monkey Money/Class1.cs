af547ec440645d8c2bccf626771fde436adb7d6e
kenx00x <kenx00x@gmail.com>
2021-03-13 18:36:44 +0100

    Add project files.

---
+++ BTD6 Infinite Monkey Money/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts.Unity;
+using Assets.Scripts.Unity.UI_New.Main;
+using Harmony;
+using MelonLoader;
+[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Money.Class1), "Infinite Monkey Money", "1.0.0", "kenx00x")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace BTD6_Infinite_Monkey_Money
+{
+    public class Class1 : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log("Infinite Monkey Money loaded!");
+        }
+        [HarmonyPatch(typeof(MainMenu), "Open")]
+        public class TitleScreen_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                Game.instance.playerService.Player.Data.monkeyMoney.Value = 999999999;
+            }
+        }
+        
+    }
+}
