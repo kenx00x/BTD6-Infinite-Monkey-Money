8a2752b64abeebf3ed79f8c2c738644dd55dd8b8
kenx00x <kenx00x@gmail.com>
2021-03-17 17:05:06 +0100

    added a way to change how much monkey money you have

--- BTD6 Infinite Monkey Money/Class1.cs
+++ BTD6 Infinite Monkey Money/Class1.cs
@@ -2,15 +2,42 @@
 using Assets.Scripts.Unity.UI_New.Main;
 using Harmony;
 using MelonLoader;
-[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Money.Class1), "Infinite Monkey Money", "1.0.0", "kenx00x")]
+using System.IO;
+[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Money.Class1), "Infinite Monkey Money", "1.1.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_Infinite_Monkey_Money
 {
     public class Class1 : MelonMod
     {
+        public static string dir = $"{Directory.GetCurrentDirectory()}\\Mods\\InfiniteMonkeyMoney";
+        public static string config = $"{dir}\\config.txt";
+        public static int amount = 999999999;
         public override void OnApplicationStart()
         {
             MelonLogger.Log("Infinite Monkey Money loaded!");
+            Directory.CreateDirectory($"{dir}");
+            if (File.Exists(config))
+            {
+                MelonLogger.Log("Reading config file");
+                using (StreamReader sr = File.OpenText(config))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        amount = int.Parse(s.Substring(s.IndexOf(char.Parse("=")) + 1));
+                    }
+                }
+                MelonLogger.Log("Done reading");
+            }
+            else
+            {
+                MelonLogger.Log("Creating config file");
+                using (StreamWriter sw = File.CreateText(config))
+                {
+                    sw.WriteLine("Amount=999999999");
+                }
+                MelonLogger.Log("Done Creating");
+            }
         }
         [HarmonyPatch(typeof(MainMenu), "Open")]
         public class TitleScreen_Patch
@@ -18,9 +45,8 @@ namespace BTD6_Infinite_Monkey_Money
             [HarmonyPostfix]
             public static void Postfix()
             {
-                Game.instance.playerService.Player.Data.monkeyMoney.Value = 999999999;
+                Game.instance.playerService.Player.Data.monkeyMoney.Value = amount;
             }
         }
-        
     }
 }
