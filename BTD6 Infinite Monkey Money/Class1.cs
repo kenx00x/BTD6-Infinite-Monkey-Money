97451de0616f21d4156a95f07233bab045efb26e
kenx00x <kenx00x@gmail.com>
2021-04-27 20:17:35 +0200

    Melonloader 0.3.0

--- BTD6 Infinite Monkey Money/Class1.cs
+++ BTD6 Infinite Monkey Money/Class1.cs
@@ -3,7 +3,7 @@ using Assets.Scripts.Unity.UI_New.Main;
 using Harmony;
 using MelonLoader;
 using System.IO;
-[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Money.Class1), "Infinite Monkey Money", "1.1.0", "kenx00x")]
+[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Money.Class1), "Infinite Monkey Money", "1.1.1", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_Infinite_Monkey_Money
 {
@@ -14,11 +14,11 @@ namespace BTD6_Infinite_Monkey_Money
         public static int amount = 999999999;
         public override void OnApplicationStart()
         {
-            MelonLogger.Log("Infinite Monkey Money loaded!");
+            MelonLogger.Msg("Infinite Monkey Money loaded!");
             Directory.CreateDirectory($"{dir}");
             if (File.Exists(config))
             {
-                MelonLogger.Log("Reading config file");
+                MelonLogger.Msg("Reading config file");
                 using (StreamReader sr = File.OpenText(config))
                 {
                     string s = "";
@@ -27,16 +27,16 @@ namespace BTD6_Infinite_Monkey_Money
                         amount = int.Parse(s.Substring(s.IndexOf(char.Parse("=")) + 1));
                     }
                 }
-                MelonLogger.Log("Done reading");
+                MelonLogger.Msg("Done reading");
             }
             else
             {
-                MelonLogger.Log("Creating config file");
+                MelonLogger.Msg("Creating config file");
                 using (StreamWriter sw = File.CreateText(config))
                 {
                     sw.WriteLine("Amount=999999999");
                 }
-                MelonLogger.Log("Done Creating");
+                MelonLogger.Msg("Done Creating");
             }
         }
         [HarmonyPatch(typeof(MainMenu), "Open")]
